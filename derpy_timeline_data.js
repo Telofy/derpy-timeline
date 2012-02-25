555879a217bce0e049c7e2a601ec1ddf62d3d021
Telofy <git@yu-shin.de>
2012-02-25 19:14:59 +0100

    Fixed formatting and added more data

--- derpy_timeline_data.js
+++ derpy_timeline_data.js
@@ -1,15 +1,26 @@
 var timeline_data = { 
-  'dateTimeFormat': 'iso8601',
-  'events': [
-    {
-       'start': '2010-10-10',
-       'durationEvent': false,
-       'title': 'Derpy’s Debute',
-       'description': 'Derpy is first seen on screen in Friendship is Magic, part 1.'
-     }, {
-       'start': '2012-02-25',
-       'durationEvent': false,
-       'title': 'Derpy’s Demise',
-       'description': 'The censored version of The Last Roundup appears on iTunes.'
-     }]
+    'dateTimeFormat': 'iso8601',
+    'events': [
+         {
+             'start': '2010-10-10',
+             'durationEvent': false,
+             'title': 'Debute',
+             'description': 'Derpy is first seen on screen in Friendship is Magic, part 1.'
+         }, {
+             'start': '2010-10-25',
+             'durationEvent': false,
+             'title': 'Naming',
+             'description': 'Dr. Foreigner on /co/ gives Derpy Hooves her name.'
+         }, {
+             'start': '2012-01-21',
+             'durationEvent': false,
+             'title': 'Canonization',
+             'description': 'Derpy becomes more canon than she already was by being called by her name in The Last Roundup.'
+         }, {
+             'start': '2012-02-25',
+             'durationEvent': false,
+             'title': 'Abduction',
+             'description': 'The censored version of The Last Roundup appears on iTunes.'
+         }
+    ]
 }
