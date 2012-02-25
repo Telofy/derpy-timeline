061d7592a155f897f95127cd679d8e2587403df5
Telofy <git@yu-shin.de>
2012-02-25 15:49:45 +0100

    Initial commit

---
+++ derpy_timeline_data.js
@@ -0,0 +1,15 @@
+var timeline_data = { 
+  'dateTimeFormat': 'iso8601',
+  'events': [
+    {
+       'start': '2010-10-10',
+       'durationEvent': false,
+       'title': 'Derpy’s Debute',
+       'description': 'Derpy is first seen on screen in Friendship is Magic, part 1.'
+     }, {
+       'start': '2012-02-25',
+       'durationEvent': false,
+       'title': 'Derpy’s Demise',
+       'description': 'The censored version of The Last Roundup appears on iTunes.'
+     }]
+}
