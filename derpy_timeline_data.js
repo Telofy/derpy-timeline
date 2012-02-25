2e492d41ba151f23bd421fcf6bd41642ffede0c7
Telofy <git@yu-shin.de>
2012-02-26 00:23:58 +0100

    More data

--- derpy_timeline_data.js
+++ derpy_timeline_data.js
@@ -7,10 +7,50 @@ var timeline_data = {
              'title': 'Debute',
              'description': 'Derpy is first seen on screen in Friendship is Magic, part 1.'
          }, {
-             'start': '2010-10-25',
+             'start': '2010-10-25 17:45:00',
              'durationEvent': false,
              'title': 'Naming',
-             'description': 'Dr. Foreigner on /co/ gives Derpy Hooves her name.'
+             'description': 'Dr. Foreigner on /co/ gives Derpy Hooves her name: “And so the legend of Derpy Hooves is born.”'
+         }, {
+             'start': '2010-11-05',
+             'durationEvent': false,
+             'title': 'Muffins',
+             'description': 'Applebuck Season airs. It is a commonly held belief that Derpy was the pony to say “muffins” in this episode.'
+         }, {
+             'start': '2010-11-14 08:44:00',
+             'durationEvent': false,
+             'title': 'Mailmare',
+             'description': 'Derpy becomes the mailmare in the fanfic <a href="http://www.derpyhoovescentral.com/2011/10/story-bright-eyes-by-manefag.html" target="_blank">Bright Eyes by Manefag</a>.'
+         }, {
+             'start': '2011-01-07',
+             'durationEvent': false,
+             'title': 'Dinky Hooves',
+             'description': 'Call of the Cutie airs and Dinky Hooves is born.'
+         }, {
+             'start': '2011-02-11',
+             'durationEvent': false,
+             'title': 'Movers',
+             'description': 'Feeling Pinkie Keen airs and… Oh, poor Twilight!'
+         }, {
+             'start': '2011-10-24',
+             'durationEvent': false,
+             'title': 'Deryp Bit',
+             'description': 'Jayson Thiessen mentions on BroNYCon that there would be a “Derpy bit” in season two.'
+         }, {
+             'start': '2011-11-01',
+             'durationEvent': false,
+             'title': 'MLP_CharH_Derpy.swf',
+             'description': 'A game on the Hub website contains an asset with “MLP_CharH_Derpy.swf” as filename.'
+         }, {
+             'start': '2011-11-30',
+             'durationEvent': false,
+             'title': 'MLP_CharW_DH_definitely_canon.swf',
+             'description': 'A game on the Hasbro website contains an asset with “MLP_CharW_DH_definitely_canon.swf” as filename.'
+         }, {
+             'start': '2011-12-17',
+             'durationEvent': false,
+             'title': 'Derpy on Facebook',
+             'description': 'The Hub mentions Derpy by name on Facebook.'
          }, {
              'start': '2012-01-21',
              'durationEvent': false,
